04fb83237cbd3731f2034284c6014ca0ba3cc1b4
DanRmna <136077976+DanRmna@users.noreply.github.com>
2023-06-23 19:43:48 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Heart)
+})
